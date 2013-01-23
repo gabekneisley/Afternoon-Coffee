69759b5cc2ee9316a103ee057be651ebbf0be0fa
Gabe Kneisley <gabekneisley@gmail.com>
2013-01-23 17:58:09 -0500

    moving to less

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 //dependencies
 var express = require('express');
-var stylus = require('stylus');
-var nib = require('nib');
+var lessMiddleware = require('less-middleware');
 
 var app = express();
 var port = 3000;
 
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib());
-}
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: compile
+
+//set up less to auto-compile
+
+app.use(lessMiddleware({
+  dest: __dirname + '/public/stylesheets',
+  src: __dirname + '/less',
+  prefix: '/stylesheets',
+  compress: true
 }));
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
